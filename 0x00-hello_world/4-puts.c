a5e8d1b7686806b5e5fa0c2e0eaaf01ec6c4f2ff
jchikwanda <chikwajacobos@gmail.com>
2022-12-08 14:40:13 +0200

    added all solutions

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: Always return (0) on success.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
