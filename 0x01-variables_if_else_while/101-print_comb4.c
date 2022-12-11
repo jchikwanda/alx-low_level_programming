723504aefa553213777d2e2213de3225c6a08027
jchikwanda <chikwajacobos@gmail.com>
2022-12-11 14:12:04 +0200

    corrected solution 12

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,45 +7,33 @@
  */
 int main(void)
 {
-	int s = 0, e = 7, s1 = 1, e1 = 8, s2 = 2, e2 = 9;
-	int tmp1 = 1, tmp2 = 2, loop = 1;
+	int s =  0, e = 7, s2 = 1, e2 = 8, s3 = 2, e3 = 9;
+	int loop = 1;
 
 	while (s <= e)
 	{
-		s1 = tmp1;
-		while (s1 <= e1)
+		s2 = s + 1;
+		while (s2 <= e2)
 		{
-			s2 = tmp2;
-			while (loop)
+			s3 = s2 + 1;
+			while (s3 <= e3)
 			{
-				if (s2 >= e2)
-					loop = 0;
-
-				if (tmp2 > e2)
-				{
-					s2 = s1 + 1;
-					tmp2 = s2;
-				}
-
 				putchar(s + '0');
-				putchar(s1 >= e1 ? e1 + '0' : s1 + '0');
-				putchar(s2 >= e2 ? e2 + '0' : s2 + '0');
+				putchar(s2 + '0');
+				putchar(s3 + '0');
 
 				if (s != e)
 				{
 					putchar(',');
 					putchar(' ');
 				}
-				s2++;
+				s3++;
 			}
-			s1++;
-			tmp2++;
-			loop = 1;
+			s2++;
 		}
 		s++;
-		tmp1++;
-		loop = 1;
 	}
 	putchar('\n');
+
 	return (0);
 }
