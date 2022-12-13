51d4889d9cd1a939b5e19b4cc515616e9ec738ee
jchikwanda <chikwajacobos@gmail.com>
2022-12-13 14:52:21 +0200

    updated betty styles of solution 9

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,8 +10,8 @@
 int print_last_digit(int num)
 {
 	int last_digit = num % 10;
-	last_digit = last_digit < 0 ? -1 * last_digit : last_digit;
 
+	last_digit = last_digit < 0 ? -1 * last_digit : last_digit;
 	_putchar(last_digit + '0');
 
 	return (last_digit < 0 ? -1 * last_digit : last_digit);
