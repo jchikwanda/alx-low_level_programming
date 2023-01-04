40116f8eacba13169574c2e593daea86651b48bc
jchikwanda <chikwajacobos@gmail.com>
2023-01-04 14:46:44 +0200

    added all solutions

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,19 @@
+/**
+ * is_prime_number - determines if a number if prime
+ * @n: number to be tested.
+ * Return: returns 1 if the input integer is a prime number
+ * Otherwise returns 0 if number is not prime.
+ */
+int is_prime_number(int n)
+{
+	int _is_prime = 0;
+
+	if (n < 2)
+		_is_prime = 0;
+	else if (n == 2)
+		_is_prime = 1;
+	else if (n % 2 == 1)
+		_is_prime = 1;
+
+	return (_is_prime);
+}
