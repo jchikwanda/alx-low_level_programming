dc7f0d292279cf9cdf16db0d809717f7cfddd0fb
jchikwanda <chikwajacobos@gmail.com>
2023-01-06 11:50:25 +0200

    added solution to 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed to program
+ * @argc: array size
+ * @argv: array containing elements
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	(void)(argv);
+	printf("%d\n", argc);
+
+	return (0);
+}
