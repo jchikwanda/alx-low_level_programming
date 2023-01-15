7994f214fad7ca4664ce539ae23f87a91510c404
jchikwanda <chikwajacobos@gmail.com>
2023-01-15 12:30:51 +0200

    added solution to 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,21 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
- * @str: string whose length is to be calculated
- *
- * Return: length of @str
- */
-unsigned int _strlen(char *str)
-{
-	unsigned int count = 0;
-
-	for ( ; str[count] != '\0'; count++)
-		;
-	return (count);
-}
-
-/**
  * string_nconcat - concatenates two strings
  * @s1: string 1
  * @s2: string 2
@@ -25,39 +10,34 @@ unsigned int _strlen(char *str)
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int s1_len = _strlen(s1), s2_len = _strlen(s2), count = 0;
-	char *text;
+	char *str;
+	unsigned int i, len, j;
+	unsigned int size;
 
-	if (!s1 && !s2)
-		return ("");
-	if (!s1)
+	len = 0;
+	if (s1 == NULL)
 		s1 = "";
-	if (!s2)
+	if (s2 == NULL)
 		s2 = "";
-
-	if (s2_len < n)
-		s2_len = n;
-
-	text = malloc((s1_len + s2_len) + 1);
-
-	if (!text)
+	while (s1[len] != '\0')
+		len++;
+	size = (len + n) * sizeof(*str);
+	str = malloc(size + 1);
+	if (str == NULL)
 		return (NULL);
-
-	while (*s1)
+	i = 0;
+	while (i < size && s1[i] != '\0')
 	{
-		text[count] = *s1;
-		s1++;
-		count++;
+		str[i] = s1[i];
+		i++;
 	}
-
-	while (*s2 != s2[s2_len - 1])
+	j = 0;
+	while (i < size && s2[j] != '\0')
 	{
-		text[count] = *s2;
-		s2++;
-		count++;
+		str[i] = s2[j];
+		i++;
+		j++;
 	}
-
-	text[count] = '\0';
-
-	return (text);
+	str[i] = '\0';
+	return (str);
 }
