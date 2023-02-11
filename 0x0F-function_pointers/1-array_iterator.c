98c5945d9e6e558559156c7d212d5ac1161b27ef
jchikwanda <chikwajacobos@gmail.com>
2023-02-11 20:48:37 +0200

    Added function to print content of array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Write a function that executes a function given as a \
+ parameter on each element of an array.
+ * @array: array to be iterated
+ * @size: size of the array
+ * @action: function to print element of array
+ *
+ * Return: Nothing on success
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		(*action)(array[i]);
+}
