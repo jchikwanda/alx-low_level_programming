443ff245c2e6343da305e75864c4eda29a74356b
jchikwanda <chikwajacobos@gmail.com>
2023-02-14 12:01:53 +0200

    Updated files

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,9 +18,9 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (i < 6 && !(*s == '\\'))
+        while (ops[i].op)
 	{
-		if (!strcmp(s, ops[i].op))
+	        if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
 		i++;
 	}
