82f3160f1addf0183a500b3dd039d820a7cd1d62
jchikwanda <chikwajacobos@gmail.com>
2023-03-15 11:10:00 +0200

    updated function

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -29,7 +29,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node = (list_t *)malloc(sizeof(list_t));
 	char *s = strdup(str);
 
-	if (!new_node)
+	if (!new_node || !s)
 		return (NULL);
 
 	new_node->str = s;
